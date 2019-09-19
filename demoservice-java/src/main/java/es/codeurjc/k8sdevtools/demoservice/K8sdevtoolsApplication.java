cb40398a61f758e0e949396a550128be98796e70
micaelgallego <micael.gallego@gmail.com>
2019-09-19 14:31:03 +0200

    Add sample projects

---
+++ demoservice-java/src/main/java/es/codeurjc/k8sdevtools/demoservice/K8sdevtoolsApplication.java
@@ -0,0 +1,13 @@
+package es.codeurjc.k8sdevtools.demoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8sdevtoolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K8sdevtoolsApplication.class, args);
+	}
+
+}
---
+++ demoservice-java/src/test/java/es/codeurjc/k8sdevtools/demoservice/K8sdevtoolsApplicationTests.java
@@ -0,0 +1,16 @@
+package es.codeurjc.k8sdevtools.demoservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class K8sdevtoolsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
